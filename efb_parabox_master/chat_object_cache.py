cf39d1c1f800197a42ab85151f603e75bc548021
ojhdt <ojhdtmail@gmail.com>
2022-11-18 19:13:20 +0800

    update

--- efb_parabox_master/chat_object_cache.py
+++ efb_parabox_master/chat_object_cache.py
@@ -1,10 +1,17 @@
 import logging
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Tuple, Dict, Iterator
+
+from ehforwarderbot.chat import BaseChat
+from ehforwarderbot.types import ModuleID, ChatID
+
+from ehforwarderbot import coordinator, Chat
+from .chat import EPMChatType, convert_chat
 
 if TYPE_CHECKING:
     from . import ParaboxChannel
 
-from ehforwarderbot import coordinator, Chat
+CacheKey = Tuple[ModuleID, ChatID]
+"""Cache storage key: module_id, chat_id"""
 
 
 class ChatObjectCacheManager:
@@ -16,6 +23,9 @@ class ChatObjectCacheManager:
         self.channel = channel
         self.db = channel.db
         self.logger = logging.getLogger(__name__)
+
+        self.cache: Dict[CacheKey, EPMChatType] = dict()
+
         self.logger.debug("Loading chats from slave channels...")
         # load all chats from all slave channels and convert to EPMChat object
         for channel_id, module in coordinator.slaves.items():
@@ -32,3 +42,38 @@ class ChatObjectCacheManager:
                 self.logger.debug("Loading chat %s from '%s'...", chat, channel_id)
                 # self.db.add_chat(chat)
             self.logger.debug("All %s chats from '%s' are enrolled.", len(chats), channel_id)
+
+    def compound_enrol(self, chat: Chat) -> EPMChatType:
+        """Convert and enrol a chat object for the first time.
+        """
+        epm_chat = convert_chat(self.db, chat)
+        self.enrol(epm_chat)
+
+        return epm_chat
+
+    def enrol(self, chat: EPMChatType):
+        """Add a chat object to the cache storage *for the first time*.
+
+        This would not update the cached object upon conflicting.
+        """
+        key = self.get_cache_key(chat)
+        self.cache[key] = chat
+        self.logger.debug("Enrolling key %s with value %s", key, chat)
+
+    @staticmethod
+    def get_cache_key(chat: BaseChat) -> CacheKey:
+        module_id = chat.module_id
+        chat_id = chat.uid
+        return module_id, chat_id
+
+    def delete_chat_object(self, module_id: ModuleID, chat_id: ChatID):
+        """Remove chat object from cache."""
+        key = (module_id, chat_id)
+        if key not in self.cache:
+            return
+        self.cache.pop(key)
+
+    @property
+    def all_chats(self) -> Iterator[EPMChatType]:
+        """Return all chats that is not a group member and not myself."""
+        return (val for val in self.cache.values() if isinstance(val, EPMChatType))
